ea3aa3b8c931215e0657884c36d604a7978a3822
vberkoz <vberkoz@gmail.com>
2023-03-18 23:41:20 +0200

    Add Noto Sans font

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,18 @@
 import '@/styles/globals.css'
 import type { AppProps } from 'next/app'
 
+import { Noto_Sans } from "@next/font/google";
+const sans = Noto_Sans({
+  subsets: ["latin", "cyrillic"],
+  style: ["normal", "italic"],
+  variable: "--font-sans",
+  weight: ["100", "200", "300", "400", "500", "600", "700"],
+});
+
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <main className={`${sans.variable} font-sans`}>
+      <Component {...pageProps} />
+    </main>
+  )
 }

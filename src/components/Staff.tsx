9ea18260773aa01e54009750dc820f782ac5c3c4
vberkoz <vberkoz@gmail.com>
2023-03-23 17:27:50 +0200

    Change Staff color schema to dark

--- src/components/Staff.tsx
+++ src/components/Staff.tsx
@@ -11,27 +11,35 @@ type Props = {
 
 export default function Staff({ data }: Props) {
   return (
-    <div className="col-span-1 lg:col-span-1 bg-gradient-to-t from-yellow-700 to-yellow-400 flex flex-col items-center p-7">
-      <p className="font-sans text-lg text-zinc-900">Актори та Персонал</p>
+    <div 
+      className="
+      col-span-1 lg:col-span-1 p-7 
+      flex flex-col items-center 
+      text-zinc-100 
+      bg-black 
+      border-b md:border-r border-zinc-800
+      "
+    >
+      <p className="font-sans font-thin text-lg">Актори та Персонал</p>
       <div className="grow min-h-[8px]"></div>
 
       <Link
         href="#"
-        className="flex flex-col items-center border border-zinc-900 md:w-[50%] rounded-full p-2 hover:bg-red-700"
+        className="flex flex-col items-center border border-zinc-800 md:w-[50%] rounded-full p-2 hover:bg-red-700"
       >
         <Image
           src={data.image.src}
           alt={data.title}
           width={200}
           height={200}
-          className="rounded-full ring-1 ring-zinc-900"
+          className="rounded-full ring-1 ring-zinc-800"
         />
       </Link>
       <div className="grow min-h-[8px]"></div>
-      <p className="font-serif font-semibold text-3xl lg:text-4xl text-zinc-900 uppercase mb-2 text-center">
+      <p className="font-serif font-semibold text-3xl lg:text-4xl uppercase mb-2 text-center">
         {data.title}
       </p>
-      <p className="font-sans text-lg text-zinc-900">{data.position}</p>
+      <p className="font-sans font-thin text-lg">{data.position}</p>
     </div>
   );
 }

a065e7c541b5abcceb75aaad0c2b2b367004115d
vberkoz <vberkoz@gmail.com>
2023-03-24 18:15:19 +0200

    Add News section

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -12,6 +12,7 @@ import Premiere from "@/components/Premiere";
 import Staff from "@/components/Staff";
 import Event from "@/components/Event";
 import Partners from "@/components/Partners";
+import Post from "@/components/Post";
 
 export default function Home() {
   return (
@@ -136,7 +137,41 @@ export default function Home() {
             topOverlayClass: "",
           }}
         />
+
         <Partners />
+
+        <div className="py-4 font-serif bg-black border-b border-zinc-800 flex flex-col items-center col-span-1 md:col-span-2 lg:col-span-4">
+          <div className="uppercase">Новини</div>
+        </div>
+        <Post data={{
+          date: "2022-09-12",
+          type: "Зустріч",
+          title: "Сучасний театр в Україні",
+          excerpt: "Хто ми? Звідки? Чи куди йдемо? Сергій Винниченко театральний блогер, автор порталу “Театральна риболовля” 18 червня, 12:00, фоє театру, 2-й поверх. Вхід вільний.",
+          containerClass: "md:border-r",
+        }} />
+        <Post data={{
+          date: "2022-12-22",
+          type: "Оголошення",
+          title: "До зустрічі у новому театральному сезоні",
+          excerpt: "Шановні глядачі, до зустрічі у новому 91-му театральному сезоні. З питань екскурсії, звертайтеся за телефоном (0372) 52-46-62.",
+          containerClass: "md:border-r-0 lg:border-r",
+        }} />
+        <Post data={{
+          date: "2022-12-22",
+          type: "Зустріч",
+          title: "Театральна імпреза “Білий птах Буковини”",
+          excerpt: "15 червня, 11:00, театральна імпреза до 80-ї річниці з дня народження видатного українського кіномитця Івана Миколайчука. Театр просто неба в музеї-садибі села Чорториї. Вхід вільний.",
+          containerClass: "md:border-r",
+        }} />
+        <Post data={{
+          date: "2022-11-22",
+          type: "Оголошення",
+          title: "Відміна вистав у жовтні 2022",
+          excerpt: "Шановні глядачі, повідомляємо про зміни в репертуарі на жовтень 2022 року, вистави з 7 по 10 жовтня - скасовано. Приносимо вибачання за незручності і дякуємо за розуміння.",
+          containerClass: "md:border-r-0 lg:border-r",
+        }} />
+        
       </div>
     </Layout>
   );

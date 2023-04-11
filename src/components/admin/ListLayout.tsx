22a42efeac9101d747394cef64020adf24fc6b0f
vberkoz <vberkoz@gmail.com>
2023-04-11 19:42:05 +0300

    Update Admin Spectacle page

--- src/components/admin/ListLayout.tsx
+++ src/components/admin/ListLayout.tsx
@@ -1,4 +1,4 @@
-import { Item } from "@/pages/admin/[page]/[id]";
+import { Item } from "@/pages/admin/page/[id]";
 import List from "./List";
 
 type Props = {
@@ -12,7 +12,7 @@ export default function ListLayout({ data }: Props) {
   return (
     <div className="grid grid-cols-3">
       <div className="flex flex-col border-r border-zinc-800 bg-zinc-900">
-        <List items={data.items} />
+        {/* <List items={data.items} /> */}
       </div>
       <div className="col-span-2 h-screen px-4 py-3">
         <div className="text-2xl">{data.currentItem.title && (data.currentItem.title) || data.currentItem.name && (data.currentItem.name)}</div>
--- src/server/routers/spectacle.ts
+++ src/server/routers/spectacle.ts
@@ -1,11 +1,42 @@
 import prisma from "@/utils/prisma";
-import { protectedProcedure, publicProcedure, router } from "../trpc";
-import { Spectacle } from "@prisma/client";
+import { protectedProcedure, router } from "../trpc";
+import { z } from "zod";
+
+const xprisma = prisma.$extends({
+  result: {
+    spectacle: {
+      href: {
+        needs: { id: true },
+        compute(spectacle) {
+          return `/admin/spectacle/${spectacle.id}`;
+        },
+      },
+    },
+  },
+});
 
 export const spectacleRouter = router({
-  list: publicProcedure.query(async () => {
-    return await prisma.spectacle.findMany({
-      take: 5
+  list: protectedProcedure.query(async () => {
+    return await xprisma.spectacle.findMany({
+      select: {
+        id: true,
+        title: true,
+        href: true,
+      },
     });
   }),
+  byId: protectedProcedure
+    .input(z.object({ id: z.string() }))
+    .query(async (req) => {
+      return await xprisma.spectacle.findUnique({
+        where: {
+          id: req.input.id,
+        },
+        select: {
+          id: true,
+          title: true,
+          href: true,
+        },
+      });
+    }),
 });

11190b1727877abbcc94c4bb5b5023efa26229d3
vberkoz <vberkoz@gmail.com>
2023-04-18 20:50:26 +0300

    Add AdminSpectacleForm component

--- src/pages/admin/spectacle/[...id].tsx
+++ src/pages/admin/spectacle/[...id].tsx
@@ -4,31 +4,34 @@ import { useRouter } from "next/router";
 import NavLayout from "@/components/admin/NavLayout";
 import pageParamToString from "@/utils/pageParamToString";
 import List from "@/components/admin/List";
+import AdminSpectacleForm from "@/components/admin/AdminSpectacleForm";
 
 export default function Spectacle() {
   const router = useRouter();
   const itemId = pageParamToString(router.query.id);
 
-  const items = trpc.spectacle.list.useQuery();
+  const items = trpc.spectacle.adminList.useQuery();
   const item = trpc.spectacle.byId.useQuery({ id: itemId });
 
   return (
     <NavLayout>
-      <div className="flex flex-col border-r border-zinc-800 bg-zinc-900">
+      <div className="flex flex-col border-r border-b border-zinc-800 bg-zinc-900">
         {!items.data ? (
           <div className="px-4 py-3">Завантаження...</div>
         ) : (
           <List items={items.data} />
         )}
       </div>
-      <div className="col-span-2 h-screen px-4 py-3">
+      <div className="col-span-2 md:h-screen px-4 py-3">
         {!item.data ? (
           <div>Завантаження...</div>
         ) : (
-          <>
-            <div className="text-2xl">{item.data.title}</div>
-            <div>{item.data.id}</div>
-          </>
+          <AdminSpectacleForm
+            data={{
+              title: item.data.title,
+              id: item.data.id,
+            }}
+          />
         )}
       </div>
     </NavLayout>

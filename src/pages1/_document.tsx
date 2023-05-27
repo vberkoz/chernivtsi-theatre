2d80f0b16bbcb366e7d044605bae53ed2c3d3709
vberkoz <vberkoz@gmail.com>
2023-05-27 23:27:25 +0300

    Fix errors 2

---
+++ src/pages1/_document.tsx
@@ -0,0 +1,13 @@
+// import { Html, Head, Main, NextScript } from 'next/document'
+
+// export default function Document1() {
+//   return (
+//     <Html lang="en">
+//       <Head />
+//       <body className="text-lg text-zinc-100 bg-black overflow-x-hidden">
+//         <Main />
+//         <NextScript />
+//       </body>
+//     </Html>
+//   )
+// }

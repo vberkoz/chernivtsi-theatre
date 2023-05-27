d371f1459c871032a1d4d8eaf3f2b9a5d47caa15
vberkoz <vberkoz@gmail.com>
2023-05-27 23:23:46 +0300

    Fix errors

--- src/pages1/_app1.tsx
+++ src/pages1/_app1.tsx
@@ -30,7 +30,7 @@ const admin = IBM_Plex_Sans({
   weight: ["100", "200", "300", "400", "500", "600", "700"],
 });
 
-function App({
+function App1({
   Component,
   pageProps,
 }: AppProps<{ session: Session }>) {

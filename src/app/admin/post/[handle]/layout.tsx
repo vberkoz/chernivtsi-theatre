730e050892e35072601e209949ea13d522410eaf
vberkoz <vberkoz@gmail.com>
2023-06-06 14:49:38 +0300

    Create Add post button

--- src/app/admin/post/[handle]/layout.tsx
+++ src/app/admin/post/[handle]/layout.tsx
@@ -1,3 +1,4 @@
+import Link from "next/link";
 import prisma from "@/utils/prisma";
 import List from "../../List";
 
@@ -18,6 +19,24 @@ export default async function RootLayout({
     <>
       <div className="flex flex-col border-r border-zinc-800 bg-zinc-900">
         <List listItems={JSON.parse(JSON.stringify(posts))} />
+        <div className="grow"></div>
+        <Link
+          href="/admin/spectacle/add"
+          className="
+            px-4 py-3 mt-14 
+            flex cursor-pointer 
+            text-left text-zinc-100
+            outline-none focus:ring-2 ring-inset ring-zinc-100 
+            bg-blue-600 hover:bg-blue-700
+          "
+        >
+          <span className="pr-4">Add post</span>
+          <div className="grow"></div>
+          <svg fill="currentColor" width="28" height="28" viewBox="0 0 32 32">
+            <path d="M17 15L17 8 15 8 15 15 8 15 8 17 15 17 15 24 17 24 17 17 24 17 24 15z"></path>
+            <title>Add</title>
+          </svg>
+        </Link>
       </div>
       {children}
     </>
